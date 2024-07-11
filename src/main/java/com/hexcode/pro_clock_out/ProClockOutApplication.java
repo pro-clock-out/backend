eaaef98844d8d93a2fc257c16a51bd854c90fea6
정재웅 <jweidon0821@gmail.com>
2024-07-11 19:57:51 +0900

    Init: 프로젝트 생성

---
+++ src/main/java/com/hexcode/pro_clock_out/ProClockOutApplication.java
@@ -0,0 +1,13 @@
+package com.hexcode.pro_clock_out;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProClockOutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProClockOutApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hexcode/pro_clock_out/ProClockOutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hexcode.pro_clock_out;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProClockOutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
