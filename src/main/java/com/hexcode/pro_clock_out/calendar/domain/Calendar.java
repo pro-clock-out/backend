ea69e6b6f7522823443fef200b8d67a1a0e2a624
wldh25 <liho858@gmail.com>
2024-07-15 17:05:03 +0900

    FIX: Label domain

--- src/main/java/com/hexcode/pro_clock_out/calendar/domain/Calendar.java
+++ src/main/java/com/hexcode/pro_clock_out/calendar/domain/Calendar.java
@@ -19,27 +19,14 @@ public class Calendar extends BaseTime {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "calendar_id")
     private Long id;
-    //private String label;
     private String title;
     private LocalDateTime startTime;
     private LocalDateTime endTime;
     private String location;
     private String notes;
 
-    @ElementCollection(fetch = FetchType.EAGER)
-    @Enumerated(EnumType.STRING)
-    private List<Label> labels = new ArrayList<>();
-
     @ManyToOne
     @JoinColumn(name="member_id")
     private Member member;
-
-    public enum Label {
-        작업,
-        휴식,
-        수면,
-        개인생활,
-        건강
-    }
 
 }
