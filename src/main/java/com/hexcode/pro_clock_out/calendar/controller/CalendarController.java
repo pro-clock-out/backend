d6541fdbca688d9721d436cdc07bb508c36414be
wldh25 <liho858@gmail.com>
2024-07-17 02:45:40 +0900

    Feat: 캘린더 상세 조회 API 생성

--- src/main/java/com/hexcode/pro_clock_out/calendar/controller/CalendarController.java
+++ src/main/java/com/hexcode/pro_clock_out/calendar/controller/CalendarController.java
@@ -1,9 +1,18 @@
 package com.hexcode.pro_clock_out.calendar.controller;
 
+import com.hexcode.pro_clock_out.calendar.dto.FindCalendarDetailResponse;
+import com.hexcode.pro_clock_out.calendar.dto.FindWeeklyCalendarResponse;
 import com.hexcode.pro_clock_out.calendar.service.CalendarService;
+import com.hexcode.pro_clock_out.global.dto.ResponseDto;
+import com.hexcode.pro_clock_out.wolibal.dto.FindTotalWolibalResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
@@ -13,4 +22,22 @@ import org.springframework.web.bind.annotation.RestController;
 public class CalendarController {
 
     private final CalendarService calendarService;
+
+//    @GetMapping("calendars")
+//    public ResponseEntity<ResponseDto> getWeeklyCalendar(Authentication authentication, @RequestParam("option") String option) {
+//        log.info("Request to get Weekly Calendar");
+//        Long memberId = (Long) authentication.getPrincipal();
+//        FindWeeklyCalendarResponse response = calendarService.findWeeklyCalendar(memberId, option);
+//        return new ResponseEntity<>(response, HttpStatus.OK);
+//    }
+
+    @GetMapping("/calendars/{calendarId}")
+    public ResponseEntity<ResponseDto> getCalendarEventDetails(Authentication authentication, @RequestParam("calendarId") Long calendarId) {
+        log.info("Request to get Calendar Event Details");
+        FindCalendarDetailResponse response = calendarService.findCalendarDetail(calendarId);
+        return new ResponseEntity<>(response, HttpStatus.OK);
+
+
+    }
+
 }
