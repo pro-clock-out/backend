804b4d529b03bb048479331c710effd9f8927d82
정재웅 <jweidon0821@gmail.com>
2024-09-10 16:50:04 +0900

    Fix: Scheduler에 Transactional 추가

--- src/main/java/com/hexcode/pro_clock_out/global/config/SchedulerConfig.java
+++ src/main/java/com/hexcode/pro_clock_out/global/config/SchedulerConfig.java
@@ -6,13 +6,16 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 @Slf4j
-@Configuration
+@Component
 @RequiredArgsConstructor
 public class SchedulerConfig {
     private final WolibalService wolibalService;
 
+    @Transactional
     @Scheduled(cron = "0 0 0 * * ?", zone = "Asia/Seoul")
     public void scheduleCreateAutoWolibal() {
         wolibalService.createDailyWolibal();
