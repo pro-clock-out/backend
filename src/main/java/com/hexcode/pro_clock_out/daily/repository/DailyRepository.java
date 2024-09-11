a4e84b96a1f9d3cc91776bdd9a05f7f91376d892
정재웅 <jweidon0821@gmail.com>
2024-09-12 03:23:36 +0900

    Refactor: 발자국 상세 조회 date Path Variable 방식으로 긴급 수정

--- src/main/java/com/hexcode/pro_clock_out/daily/repository/DailyRepository.java
+++ src/main/java/com/hexcode/pro_clock_out/daily/repository/DailyRepository.java
@@ -4,9 +4,12 @@ import com.hexcode.pro_clock_out.daily.domain.Daily;
 import com.hexcode.pro_clock_out.member.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 public interface DailyRepository extends JpaRepository<Daily, Long> {
     List<Daily> findDailyByMember(Member member);
 
+    Optional<Daily> findDailyByDateAndMember(LocalDate date, Member member);
 }
